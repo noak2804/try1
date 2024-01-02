1fb16a4a48204b55d50b34e6d0b79ad44c31373d
noak2804 <noak2804@gmail.com>
2024-01-02 09:48:04 +0200

    updates

--- app/src/main/java/com/example/theproject/MainRecipes/MainRecipesActivity.java
+++ app/src/main/java/com/example/theproject/MainRecipes/MainRecipesActivity.java
@@ -1,10 +1,7 @@
 package com.example.theproject.MainRecipes;
 
-import static com.example.theproject.R.id.toolbar;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,6 +28,7 @@ public class MainRecipesActivity extends AppCompatActivity implements MainRecipe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_recipes);
         presenter=new MainRecipesPresenter(this);
+
 
     }
     public void setRecyclerBest(ArrayList<RecipeInformation> recipes)
