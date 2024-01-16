b7b7b9a7e3dfb8ac269648733a3bc5cf1975e163
noak2804 <noak2804@gmail.com>
2024-01-16 08:20:08 +0200

    FireBase-User

---
+++ app/src/main/java/com/example/theproject/model/User.java
@@ -0,0 +1,16 @@
+package com.example.theproject.model;
+
+public class User {
+    String name;
+    String email;
+    Integer phone;
+    String password;
+    public User()
+    {
+        name="";
+        email="";
+        phone=0;
+        password="";
+    }
+
+}
